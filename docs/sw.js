febe509c33aa1b62103d1e95d074a38093c110c1
antwan <antwanvdm@gmail.com>
2019-07-17 21:54:51 +0200

    Fooling around with service worker

--- docs/sw.js
+++ docs/sw.js
@@ -1,3 +1,10 @@
 self.addEventListener('install', function (event) {
     console.log("install!");
 });
+
+self.addEventListener('fetch', function (event) {
+    let request = event.request;
+    if (request.url.indexOf('api/') !== -1) {
+        return;
+    }
+});
